39e82ed675f38ebf5bc9f62317cd13a504fed0a6
beMimg <bernardommguerreiro@gmail.com>
2024-03-13 19:12:26 +0100

    loading on Post handled

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,9 +2,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CommentSection from "../components/CommentSection";
+import Loading from "../components/Loading";
 
 export default function Post() {
   const [post, setPost] = useState();
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const { post_id } = useParams();
 
   useEffect(() => {
@@ -15,39 +19,48 @@ export default function Post() {
         );
         setPost(response.data.post);
         return;
-      } catch (err) {}
+      } catch (err) {
+        return;
+      } finally {
+        setIsLoading(false);
+        return;
+      }
     };
     getPosts();
   }, [post_id]);
 
-  return (
-    post && (
-      <div className="font-sans animate-fade-in flex items-center justify-center">
-        <div className=" self-center lg:w-[960px]">
-          <div className="flex flex-col gap-12 p-5">
-            <div>
-              <h1 className="text-2xl font-semibold">{post.title}</h1>
-              <div className="flex flex-row gap-2">
-                <p>Topics:</p>
-                <p className="">{post.topic}</p>
-              </div>
+  return post && isLoading === false ? (
+    <div className="font-sans animate-fade-in flex items-center justify-center">
+      <div className=" self-center lg:w-[960px]">
+        <div className="flex flex-col gap-12 p-5">
+          <div>
+            <h1 className="text-2xl font-semibold">{post.title}</h1>
+            <div className="flex flex-row gap-2">
+              <p>Topics:</p>
+              <p className="">{post.topic}</p>
             </div>
-            <img
-              className="flex self-center  rounded-md object-cover object-center lg:h-[500px] lg:w-[90%]"
-              src={post.imgSrc}
-              alt=""
-            />
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row gap-2">
-                <p>Author: </p>
-                <p className="font-semibold">{post.author.username}</p>
-              </div>
-              <p className="text-sm ">{post.formated_date}</p>
-            </div>
-            <p className="text-md leading-relaxed md:text-lg">{post.text}</p>
           </div>
-          <CommentSection className="animate-fade-in" post_id={post_id} />
+          <img
+            className="flex self-center  rounded-md object-cover object-center lg:h-[500px] lg:w-[90%]"
+            src={post.imgSrc}
+            alt=""
+          />
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-row gap-2">
+              <p>Author: </p>
+              <p className="font-semibold">{post.author.username}</p>
+            </div>
+            <p className="text-sm ">{post.formated_date}</p>
+          </div>
+          <p className="text-md leading-relaxed md:text-lg">{post.text}</p>
         </div>
+        <CommentSection className="animate-fade-in" post_id={post_id} />
+      </div>
+    </div>
+  ) : (
+    isLoading && (
+      <div className="flex flex-1 items-center justify-center">
+        <Loading />
       </div>
     )
   );
