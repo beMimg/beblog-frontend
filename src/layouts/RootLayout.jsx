b6af0b75ed2b5c2d2f52a0d03d71092dea044d16
beMimg <bernardommguerreiro@gmail.com>
2024-03-14 23:32:19 +0100

    changed Layout component name to RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
-import Outlet from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
 
-export default function Layout() {
+export default function RootLayout() {
   return (
     <div className="flex min-h-screen flex-col  font-inter">
       <header className="themeModalButton the sticky top-0 z-20 animate-fade-in border-b-2 border-blue-500 p-4">
